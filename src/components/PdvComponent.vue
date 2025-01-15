8917397cc60d61eacae3f93988148c298ecc51d0
Vitor <vitornw10gmail.com>
2025-01-15 16:34:01 -0300

    products already being pushed in cart!

--- src/components/PdvComponent.vue
+++ src/components/PdvComponent.vue
@@ -11,7 +11,8 @@
             label="Select a Client"
             :items="getAllClients"
             item-text="nome"   
-            item-value="id"
+            item-value="id" 
+            v-model="selectedClient"
           ></v-select>
         </v-col>    
       </v-row>
@@ -70,34 +71,36 @@
     v-model="selected"
     :headers="headers"
     :items="filteredProducts"
-    :single-select="singleSelect"
     item-key="id"
-    show-select
-    class="elevation-1"
-   
-  >
-    <template v-slot:top>
-      <v-switch
-        v-model="singleSelect"
-        label="Single select"
-        class="pa-3"
-      ></v-switch>
-    </template>
+    class="elevation-1"   
+  >    
 
     <template v-slot:[`item.actions`]="{ item }">
-        <v-btn
-          icon
-          
-          :disabled="item.estoque === 0"
-        >
-          <v-icon>mdi-cart-plus</v-icon>
-        </v-btn>
+      <v-btn
+        icon
+        @click="openModal(item)"
+      >
+        <v-icon>mdi-eye</v-icon>
+      </v-btn>
     </template>
     
   </v-data-table> 
 
-  
+  <v-dialog v-model="modal">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Detalhes do Produto</span>
+      </v-card-title>
 
+      <v-text-field label="Quantity" v-model="selectedProductQuantity"></v-text-field>      
+      {{ cart }}
+      <v-card-actions>
+        <v-btn text @click="closeModal">Fechar</v-btn>
+        <v-btn text @click="addToCart">Add to Cart</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  
   </div>
 </template>
 
@@ -108,10 +111,15 @@ export default {
     name: 'PdvComponent',
     data () {
       return {
+        selectedClient: '',
         searchQuery: '',
         cart: [],
         singleSelect: true,
         selected: [],
+        modal: false,  
+        selectedProduct: {        
+        },
+        selectedProductQuantity: 0,
         headers: [
           {
             text: 'Name',
@@ -125,37 +133,59 @@ export default {
           { text: 'Height', value: 'altura' },
           { text: 'Stock', value: 'estoque' },
           { text: 'Ações', value: 'actions' },
-        ],        
+        ],
+        
       }
     },
     methods: {
-        ...mapActions({
-            setAllClients: 'setAllClients',
-            setAllProducts: 'setAllProducts'
-        }),
+      ...mapActions({
+          setAllClients: 'setAllClients',
+          setAllProducts: 'setAllProducts'
+      }),
 
+      openModal(product) {
+        this.selectedProduct = product; // Armazena o produto selecionado
+        this.modal = true; // Abre o modal
+      },    
+      
+      closeModal() {
+        this.modal = false
       },
-      computed: {
-        ...mapState({
-          allProducts: state => state.products
-        }),
-        ...mapGetters({
-          getAllClients: 'getAllClients',
-          getAllProducts: 'getAllProducts'            
-        }),
-        
-        filteredProducts() {
-          return this.allProducts.filter(product => product.nome.toLowerCase().includes(this.searchQuery.toLowerCase()))
+
+      addToCart() {
+        if(this.selectedProductQuantity > this.selectedProduct.estoque) {
+          console.log('quantity higher then the stock')
+        } else { //ok          
+          this.selectedProduct.estoque -= this.selectedProductQuantity
+          
+          //this.cart.push(this.selectedProduct)          
         }
-
-
+       
+      }
+       
     },
+    computed: {
+      ...mapState({
+        allProducts: state => state.products
+      }),
+      ...mapGetters({
+        getAllClients: 'getAllClients',
+        getAllProducts: 'getAllProducts'            
+      }),
+      
+      filteredProducts() {
+        return this.allProducts.filter(product => product.nome.toLowerCase().includes(this.searchQuery.toLowerCase()))
+      },
+
+      
+    },
+
     mounted() {
         this.setAllClients()
         this.setAllProducts()
-      },
+    },
       
-      created() {
+    created() {
     }
 }
 </script>
